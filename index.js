8fb0fade08b9d11072016804f06a098f6022afcf
rmejia-tajc <rmejia.tajc@gmail.com>
2019-02-22 12:28:18 -0700

    complete MVP and one stretch goal

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const db = require('./projectTrackerHelpers.js')
+const dbAccess = require('./projectTrackerHelpers.js')
 
 const server = express();
 
@@ -11,7 +11,61 @@ server.use(express.json());
 
 
 
+// error responses
+const errors = {
+    '19': 'Another record with that value exists',
+};
 
+
+// endpoints
+
+
+server.post('/api/projects', (req,res) => {
+    const project = req.body;
+    dbAccess.addProject(project)
+        .then(id => {
+        res
+            .status(201)
+            .json(id)
+        })
+        .catch(err => {
+        res
+            .status(500)
+            .json({message: 'Failed to add project'})
+        })
+})
+
+server.post('/api/actions', (req,res) => {
+    const action = req.body;
+    dbAccess.addAction(action)
+        .then(id => {
+        res
+            .status(201)
+            .json(id)
+        })
+        .catch(err => {
+        res
+            .status(500)
+            .json({message: 'Failed to add action'})
+        })
+})
+
+server.get('/api/projects/:id/actions', async (req, res) => {
+
+    try {
+        const {id} = req.params;  
+        const project = await dbAccess.getActionsByProject(id);
+
+        if (project) {
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ message : 'The entry with that specified ID does not exist'});
+        }
+
+    } catch (error) {
+    res.status(500).json(error);
+    }
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n*** running on ${port} ***\n`));
--- projectTrackerHelpers.js
+++ projectTrackerHelpers.js
@@ -7,6 +7,37 @@ const db = knex(knexConfig.development);
 
 module.exports = {
     
+    addProject: (project) => {
+        return db('projects').insert(project)
+    },
+    
+    addAction: (action) => {
+        return db('actions').insert(action)
+    },  
+
+	getActionsByProject: (id) => {
+
+		const project = db('projects')
+			.select()
+			.where('id', id)
+			.first()
+
+		const actions = db('actions')
+			.select()
+			.where('project_id', id)
+
+		return Promise.all([project, actions]).then(results => {
+			let [project, actions] = results;
+			return {
+				id: project.id,
+				name: project.name,
+				description: project.description,
+				completed: project.completed,
+				actions: actions
+			};
+		});
+	}
+    
 
   
   }
